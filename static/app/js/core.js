d05aaa3e66d0622bbcca3dd970fb52943e3ffe6c
Breno Silva <brenokcc@yahoo.com.br>
2023-10-22 23:58:53 -0300

    Continuação da implementação

--- static/app/js/core.js
+++ static/app/js/core.js
@@ -29,6 +29,7 @@ function request(method, url, callback, data){
         }
     ).then(
         text => {
+            console.log(httpResponse.headers.get('Content-Type'));
             var data = JSON.parse(text||'{}');
             if(data.redirect) document.location.href = data.redirect;
             callback(data, httpResponse);
